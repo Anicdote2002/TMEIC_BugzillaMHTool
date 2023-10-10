4e8a140353be177f976e7a21a56218c02c0ca539
Aniruddh <aniruddh2002@vt.edu>
2023-10-10 06:49:59 -0400

    sample combobox

---
+++ WinFormsApp_Test_autocomplete/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WinFormsApp_Test_autocomplete
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox_CompTech = new ComboBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // comboBox_CompTech
+            // 
+            comboBox_CompTech.FormattingEnabled = true;
+            comboBox_CompTech.Location = new Point(349, 133);
+            comboBox_CompTech.Name = "comboBox_CompTech";
+            comboBox_CompTech.Size = new Size(321, 28);
+            comboBox_CompTech.TabIndex = 0;
+            comboBox_CompTech.KeyPress += comboBox_CompTech_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(268, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Username";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(349, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(321, 27);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "Aniruddh.Chauhan@tmeic.com";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(349, 100);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(321, 27);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "tmca1117C";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(273, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(291, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Emails";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(349, 167);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Get User Emails";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(comboBox_CompTech);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox_CompTech;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp_Test_autocomplete/Form1.cs
@@ -0,0 +1,318 @@
+using System;
+using System.Windows.Forms;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using System.Collections.Specialized;
+using System.Web;
+using System.Xml;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Security.Policy;
+using HtmlAgilityPack;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WinFormsApp_Test_autocomplete
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<object> comboBox_CompTech_ItemsCopy = new List<object>();
+        private void comboBox_CompTech_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            string userInput = comboBox_CompTech.Text;
+            List<object> suggestions = GetAutocompleteSuggestions(userInput); // Implement this method to retrieve suggestions.
+            if(char.IsLetterOrDigit(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsPunctuation(e.KeyChar))
+    {
+                // Handle printable characters (letters, numbers, symbols, punctuation).
+                comboBox_CompTech.Items.Clear();
+                comboBox_CompTech.Items.AddRange(suggestions.ToArray()); // Add suggestions to ComboBox items.
+                comboBox_CompTech.SelectionStart = userInput.Length; // Keep the user's input selected.
+                comboBox_CompTech.DroppedDown = true; // Show the dropdown with suggestions.
+                comboBox_CompTech.Refresh();
+            }
+            else if (string.IsNullOrEmpty(userInput))
+            {
+                // Hide the dropdown when there's no input.
+                comboBox_CompTech.Items.Clear();
+                 comboBox_CompTech.DroppedDown = false;
+                comboBox_CompTech.Refresh();
+            }
+        }
+        private List<object> GetAutocompleteSuggestions(string userInput)
+        {
+            List<object> suggestions = new List<object>();
+            //// Iterate through the existing items in the ComboBox.
+            foreach (object item in comboBox_CompTech_ItemsCopy)
+            {
+                // string itemAsString = 
+
+                // Filter items that start with the user's input (case-insensitive).
+                if (item.ToString().Contains(userInput))
+                {
+                    suggestions.Add(item);
+                }
+            }
+            return suggestions;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (get_users_email())
+            {
+                if (MessageBox.Show("Email List has been update successfully! Proceed?", "Visit", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
+                {
+
+                    // comboBox_CompTech.AutoCompleteMode = AutoCompleteMode.Suggest;
+                    //   comboBox_CompTech.AutoCompleteSource = AutoCompleteSource.ListItems;
+                }
+            }
+        }
+        //private void comboBox_CompTech_TextChanged(object sender, EventArgs e)
+        //{
+        //    HandleTextChanged();
+        //    comboBox_CompTech.DataSource = comboBox_CompTech.Items;
+
+        //}
+
+        //private void HandleTextChanged()
+        //{
+        //    var txt = comboBox_CompTech.Text;
+        //    var list = from string d in comboBox_CompTech_ItemsCopy
+        //               where d.ToUpper().StartsWith(comboBox_CompTech.Text.ToUpper())
+        //               select d;
+        //    if (list.Count() > 0)
+        //    {
+        //        comboBox_CompTech.DataSource = list.ToList();
+        //        var sText = comboBox_CompTech.Items[0].ToString();
+        //        comboBox_CompTech.SelectionStart = txt.Length;
+        //        comboBox_CompTech.SelectionLength = sText.Length - txt.Length;
+        //        comboBox_CompTech.DroppedDown = true;
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        comboBox_CompTech.DroppedDown = false;
+        //        comboBox_CompTech.SelectionStart = txt.Length;
+        //    }
+        //}
+
+        //private void comboBox_CompTech_KeyUp(object sender, KeyEventArgs e)
+        //{
+        //    if (e.KeyCode == Keys.Back)
+        //    {
+        //        int sStart = comboBox_CompTech.SelectionStart;
+        //        if (sStart > 0)
+        //        {
+        //            sStart--;
+        //            if (sStart == 0)
+        //            {
+        //                comboBox_CompTech.Text = "";
+        //            }
+        //            else
+        //            {
+        //                comboBox_CompTech.Text = comboBox_CompTech.Text.Substring(0, sStart);
+        //            }
+        //        }
+        //        e.Handled = true;
+        //    }
+        //}
+        private bool get_users_email()
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            string url = "https://tools.tmeic.com/mh/editusers.cgi?action=list&matchvalue=login_name&matchstr=&matchtype=substr&groupid=1&is_enabled=1";
+            string loginToken;
+            string email_cookies;
+            string response;
+            try
+            {
+                if (!username.Contains("@"))
+                {
+                    MessageBox.Show("Make sure that the entered usernames and those in the ProjectEmail.xml file are of the form 'first.last@tmeic.com' or are correct", "Friendly reminder!");
+                    return false;
+                }
+                username = username.Replace("@", "%40").ToLower();  // Replace @ with %40 and make sure it is lower case
+                if (username == "" || password == "")
+                {
+                    MessageBox.Show("Please fill out all the fields", "Friendly reminder!");
+                    return false;
+                }
+                using (WebClient web = new WebClient())
+                {
+                    if (password == "")
+                    {
+                        MessageBox.Show("You must enter your Bugzilla credentials to test the cookies.", "Error!");
+                        return false;
+                    }
+                    try
+                    {
+                        try   //Load XML Document
+                        {
+                            System.Xml.XmlDocument DocTest = new System.Xml.XmlDocument();
+                            DocTest.Load(@"ProjectEmail.xml");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Please make sure that the ProjectEmail.xml file is located in the same file as this tool when it is run.", "Error");
+                            return false;
+                        }
+                        System.Xml.XmlDocument Doc = new System.Xml.XmlDocument();
+                        Doc.Load(@"ProjectEmail.xml");
+                        XmlNode curNode = Doc.SelectSingleNode("Configuration/FieldEngineerManager");
+                        curNode = Doc.SelectSingleNode("Configuration/Cookies");
+                        if (curNode == null)
+                        {
+                            MessageBox.Show("Problem with ProjectEmail.xml/Configuration/Cookies");
+                            return false;
+                        }
+                        email_cookies = curNode.InnerText;
+                        curNode = Doc.SelectSingleNode("Configuration/LoginToken");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An exception was thrown while reading the ProjectEmail.xml. Please insure that it is in the same folder as the project creation utility. ", "Error!");
+                        return false;
+                    }
+                    try
+                    {
+                        web.Headers.Add(HttpRequestHeader.Cookie, email_cookies);
+                        CredentialCache credentialCache = new CredentialCache();
+                        credentialCache.Add(new System.Uri(url), "Basic", new NetworkCredential(username, password, "tools.tmeic.com"));
+                        try
+                        {
+                            web.Credentials = credentialCache;
+                            web.BaseAddress = url;
+                            string token_page = web.DownloadString(url);  //retrieve a login webpage as a string                          
+                            string _buggzilla_login = "Bugzilla_login_token";
+                            int position = 0;
+                            for (int i = 0; i < token_page.Length; i++)//iterate through the string until finding the login token
+                            {
+                                bool done_flag = false; //flag to track if the vlaue is found and terminate the for loop
+                                for (int j = 0; j < _buggzilla_login.Length; j++)
+                                {//for every character, begin a substring search
+                                    if (_buggzilla_login[j] != token_page[i + j])
+                                    {//sequentially check each character against the phrase "Bugzilla_login_token", if there is a mis-match, break and start over from next character
+                                        break;
+                                    }
+                                    else if (j == _buggzilla_login.Length - 1)
+                                    {//if all cards match, position becomes i;
+                                        position = i;
+                                        done_flag = true; //set the finish flag to false
+                                        break;
+                                    }
+                                }
+                                if (done_flag) { break; }//if the token position was found, break and end the position finding loop.
+                            }
+                            string _value = "value"; //jump position to first instance of value after the token is found.
+                            for (int i = position; i < token_page.Length; i++)//iterate through the string until finding the login token
+                            {
+                                bool done_flag = false; //flag to track if the value is found and terminate the for loop
+                                for (int j = 0; j < _value.Length; j++)
+                                {//for every character, begin a substring search
+                                    if (_value[j] != token_page[i + j])
+                                    {//sequentially check each character against the phrase "Bugzilla_login_token", if there is a mis-match, break and start from next character
+                                        break;
+                                    }
+                                    else if (j == _value.Length - 1)
+                                    {//if all cards match, position becomes i;
+                                        position = i + j + 3; //set the position to four characters pasts the "e" in value, the first character of the login token
+                                        done_flag = true;
+                                        break;
+                                    }
+                                }
+                                if (done_flag) { break; }//if the token position was found, break and end the position finding loop.
+                            }
+                            StringBuilder loginToken_temp = new StringBuilder();
+                            while (token_page[position] != 34)
+                            {
+                                loginToken_temp.Append(token_page[position]);
+                                position++;
+                            }
+                            loginToken = loginToken_temp.ToString();
+                            response = web.UploadString(url, "POST", "Bugzilla_login=" + username + "&Bugzilla_password=" + password + "&Bugzilla_login_token=" + loginToken + "&GoAheadAndLogIn=Log+in");
+                            if (response.Contains("<title>Untrusted Authentication Request</title>"))
+                            {
+                                MessageBox.Show("Bugzilla returned the Untrusted Authentication Request page. The Cookies or Login Tokens are invalid", "Failed!");
+                                return false;
+                            }
+                            else if (response.Contains("<title>Invalid Username Or Password</title>"))
+                            {
+                                MessageBox.Show("Invalid Username Or Password.");
+                                return false;
+                            }
+                            else if (response.Contains("<title>Authorization Required</title>"))
+                            {
+                                MessageBox.Show("Authorization Required. You are not an Admin!");
+                                return false;
+                            }
+                            else if (response.Contains("<title>Select user</title>"))
+                            {
+                                MessageBox.Show("Select User Emails");
+                                HtmlAgilityPack.HtmlDocument html_doc = new HtmlAgilityPack.HtmlDocument();
+                                html_doc.LoadHtml(response);
+                                HtmlAgilityPack.HtmlNodeCollection thNode = html_doc.DocumentNode.SelectNodes("//table[@id='admin_table']//td//a");
+                                if (thNode != null)
+                                {
+                                    // Iterate through all child nodes of the <th> element
+                                    //  comboBox_CompTech.BeginUpdate();
+                                    //     comboBox_ProjMan.BeginUpdate();  
+
+                                    foreach (HtmlNode childNode in thNode)
+                                    {
+                                        // Extract the content of child nodes (text or HTML)
+                                        string elementContent = childNode.InnerHtml; // or .InnerText for plain text                                       
+                                        if (elementContent.Contains("&#64;"))
+                                        {
+                                            elementContent = elementContent.Replace("&#64;", "@");
+                                            string Comp = elementContent;
+                                            // comboBox_CompTech.Items.Add(elementContent);
+                                            comboBox_CompTech_ItemsCopy.Add(elementContent);
+
+                                        }
+                                    }
+                                }
+                                return true;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Unexpected response");
+                                return false;
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Exception thrown. Operation failed");
+                            return false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Failed to complete!" + e, "Error");
+                        return false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Failed to complete! Check Bugzilla to see if any part of the project was created. \r\n\r\n Exception thrown: " + e, "Error!");
+                return false;
+            }
+        }
+
+
+    }
+}
